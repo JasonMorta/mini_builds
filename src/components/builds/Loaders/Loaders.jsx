c53c1947ad77587f44c5cecc6913ff280ee3e796
JasonMorta <jason.morta23@gmail.com>
2022-10-06 22:55:29 +0200

    updates

---
+++ src/components/builds/Loaders/Loaders.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from 'react'
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import './loaders.css';
+
+export default function Loaders() {
+
+ const [currentTime, setCurrentTime ] = useState(0);
+ const [loadText, setLoadText] = useState("Loading...")
+
+
+ const tick = ()=>{
+
+  //Stop weh bar is full 
+  if (currentTime !== 100){ 
+   setCurrentTime(state => state + 1)
+  }
+
+  //Change text when full
+  if (currentTime === 100){
+   setLoadText("Completed")
+  }
+ }
+ 
+ //Increment every 20ns
+  useEffect(() => {
+   const timer = setInterval( tick, 20 );
+   return () => clearInterval(timer);
+ }, [tick] );
+
+//Use styled-components package to access the ::before attribute
+ const H3 = styled.h3`
+  font-weight: bold;
+  &:before {
+   width: ${currentTime}%
+ }
+`;
+
+
+  return (
+    <div>
+     <p>useEffect with setInterval trick</p>
+     <h1>{currentTime}%</h1>
+     <H3 className='h3Style' >
+    {loadText}
+     </H3>
+    </div>
+  )
+}
