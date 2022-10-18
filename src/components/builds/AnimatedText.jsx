cac6787a4db00283dcc837b3ad2307be8e23b864
JasonMorta <jason.morta23@gmail.com>
2022-10-18 16:57:55 +0200

    updates

--- src/components/builds/AnimatedText.jsx
+++ src/components/builds/AnimatedText.jsx
@@ -5,11 +5,30 @@ import './AnimatedText.css'
 export default function AnimatedText() {
 
   const [state, setState]= useState("Animated Text")
+  const [stay, setStay]= useState(false)
 
 let speed = 0.5;
 
 function textInput(e){
 setState(e.target.value)
+}
+
+//keeps the selected letter down
+function stayDown(e){
+  
+  if (e.target.className === "text-pop-up-top") {
+      e.target.className = "text-pop-up-top stayDown"
+      setStay(true)
+      e.target.style.color = "hotpink"
+      } else {
+        e.target.className = "text-pop-up-top"
+        e.target.style.color = "#f8f8ff"
+        setStay(false)
+      }
+  console.log(e.target.className)
+  //setStay(prev => !prev)
+  console.log(stay)
+  setStay(true)
 }
 
 //console.log(state.replaceAll(" ", "").split(""))
@@ -28,8 +47,9 @@ setState(e.target.value)
       <div className='animated_text'>
          
       {state.split("").map((item, index) =>(
-       <h1 
-        className='text-pop-up-top'
+       <h1 onClick={stayDown}
+
+        className="text-pop-up-top"
         key={index}
         style={{'animationDuration': `${speed}s`}}>{item}</h1>
        ))}
--- src/components/tiles/Tile.jsx
+++ src/components/tiles/Tile.jsx
@@ -1,28 +1,72 @@
 import React from 'react'
+import { useEffect } from 'react';
 import { useState } from 'react';
 import "./tiles.css"
 
 export default function Tile() {
   const [tiles, setTiles] = useState(10);
+  const [bgc, setBgc] = useState("#FF6F61")
+  const [count, setCount] = useState(1)
 
   let divs = [];
   let inc = 0;
+
+  function clickTile(e){
+    e.target.style.backgroundColor = "#2196f3"
+    console.log(e)
+  }
 
   for (let i = 0; i < 6; i++) {
     let RN = Math.random().toFixed(2);
     divs.push(
       <div
+        onAnimationStart={animationStart }
+        onAnimationEnd ={animationEnd}
+        onAnimationIteration={animationIteration}
         className="tile"
+        onClick={clickTile}
         style={{
           left: `${0 + inc}px`,
           animationDelay: `${RN}s`,
+          backgroundColor: bgc,
+          animationIterationCount: count,
         }}
       >
         {tiles}
       </div>
     );
     inc += 105;
+
+
+  function animationStart(){
+    console.log(bgc);
+    setBgc("")
+    setBgc("#FF6F61")
   }
+
+  function animationEnd(){
+    console.log("ended")
+    
+    setBgc("#FF6F61")
+    if (count < 5) {
+      setBgc("#FF6F61")
+      setCount(prev => prev+1)
+    }
+  }
+
+  function animationIteration(){
+    
+    setBgc("#FF6F61")
+  
+  }
+}
+   //keep track of tile position
+  //  useEffect(() => {
+  //   const timer = setInterval( tick, 20 );
+  //   return () => clearInterval(timer);
+  // }, [tick] );
+
+  
 
   return (
     <div className="tile-container">
