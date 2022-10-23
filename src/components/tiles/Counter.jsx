ed47721b62be9b9058056411494e5fda9a0db30e
JasonMorta <jason.morta23@gmail.com>
2022-10-23 15:56:18 +0200

    update

---
+++ src/components/tiles/Counter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function Counter(props) {
+
+ const [selectCounter, setSelectCounter] = useState(props.LCounter)
+ console.log(selectCounter);
+  return (
+    <p className='select-counter'>You score: {selectCounter}</p>
+  )
+}
--- src/components/tiles/Tile.jsx
+++ src/components/tiles/Tile.jsx
@@ -1,78 +1,148 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { useState } from 'react';
-import "./tiles.css"
+import React from 'react';
+import { useState, useEffect, useRef } from 'react';
+import "./tiles.css";
 
 export default function Tile() {
-  const [tiles, setTiles] = useState(10);
+  const [tiles, setTiles] = useState("Card");
   const [bgc, setBgc] = useState("#FF6F61")
-  const [count, setCount] = useState(1)
+  const [count, setCount] = useState(112)
+  const [totalCards, setTotalCards] = useState(0)
+  const [speed, setSpeed] = useState(3)
+  const selectedCount = useRef(0);
 
-  let divs = [];
-  let inc = 0;
+  let divs = [];// holds all teh cards
+  let divs2 = [];
+  let divs3 = [];
+  let cardCounter = 0;
+  let inc = 0;//used to set the distance between each card
 
+  // useEffect(() => {
+  //   selectedCount.current =  selectedCount.current +1
+  
+  // }, [])
+  
 
+//create each card with loop
+  for (let i = 0; i < 4; i++) {
 
-  for (let i = 0; i < 6; i++) {
-
-    let RN = Math.random().toFixed(2);
-    divs.push(
+    //gives each card a random start time(0-1sec)
+  divs.push(
       <div
         onAnimationStart={animationStart }
         onAnimationEnd ={animationEnd}
         onAnimationIteration={animationIteration}
         className="tile"
-        onClick={(e)=> {e.target.style.backgroundColor = "#2196f3"}}
+        onMouseDown={(e)=> {
+          e.target.style.backgroundColor = "rgb(33, 150, 243)"
+          //setSelectCounter(prev => prev +1)
+          selectedCount.current++
+          console.log( selectedCount.current);
+        }}
+
+        
         style={{
           left: `${0 + inc}px`,
-          animationDelay: `${RN}s`,
+          animationDelay: `0.${Math.floor(Math.random() * 4)}s`,
           backgroundColor: bgc,
           animationIterationCount: count,
+          animationDuration: `${speed}s`
         }}
       >
-        {tiles}
+        
       </div>
     );
+  divs2.push(
+      <div
+        onAnimationStart={animationStart }
+        onAnimationEnd ={animationEnd}
+        onAnimationIteration={animationIteration}
+        className="tile"
+        onMouseDown={(e)=> {
+          e.target.style.backgroundColor = "rgb(33, 150, 243)"
+          //setSelectCounter(prev => prev +1)
+          selectedCount.current++
+          console.log( selectedCount.current);
+        }}
+
+        
+        style={{
+          left: `${0 + inc}px`,
+          animationDelay: `1.${Math.floor(Math.random() * 4)}s`,
+          backgroundColor: bgc,
+          animationIterationCount: count,
+          animationDuration: `${speed}s`
+        }}
+      >
+        
+      </div>
+    );
+  divs3.push(
+      <div
+        onAnimationStart={animationStart }
+        onAnimationEnd ={animationEnd}
+        onAnimationIteration={animationIteration}
+        className="tile"
+        onMouseDown={(e)=> {
+          e.target.style.backgroundColor = "rgb(33, 150, 243)"
+          //setSelectCounter(prev => prev +1)
+          selectedCount.current++
+          
+       
+         
+        }}
+
+        
+        style={{
+          left: `${0 + inc}px`,
+          animationDelay: `2.${Math.floor(Math.random() * 4)}s`,
+          backgroundColor: bgc,
+          animationIterationCount: count,
+          animationDuration: `${speed}s`
+        }}
+      >
+        
+      </div>
+    );
+
+
     inc += 105;
 
-
+    //Animation start Event
   function animationStart(){
-    console.log(bgc);
+    //console.log(bgc);
     setBgc("")
     setBgc("#FF6F61")
   }
 
+  //Animation End event
   function animationEnd(){
     console.log("ended")
-    
-    setBgc("#FF6F61")
-    if (count < 5) {
-      setBgc("#FF6F61")
-      setCount(prev => prev+1)
-    }
+    setTotalCards(selectedCount.current)
   }
 
-  function animationIteration(){
+  //Animation each iteration event
+  //reset the bgc of the selected card on each loop
+  // eslint-disable-next-line no-loop-func
+  function animationIteration(e){
+    e.target.style.backgroundColor = "#FF6F61";
     
-    setBgc("#FF6F61")
-    
-    console.log(bgc);
-  
+    console.log(selectedCount.current);
   }
 }
-   //keep track of tile position
-  //  useEffect(() => {
-  //   const timer = setInterval( tick, 20 );
-  //   return () => clearInterval(timer);
-  // }, [tick] );
 
   
 
   return (
     <div className="tile-container">
+      <div className='hide-cards-top'></div>
       {divs}
+      {divs2}
+      {divs3}
+   
+      <p className='select-counter'>You score: {cardCounter}</p>
 
       <div className="tile-column1"> </div>
+      <div className='hide-cards-bot'></div>
     </div>
   );
 }
