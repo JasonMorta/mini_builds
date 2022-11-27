31ab6105e6c99f87c6045b2dbf5c581996d394d5
Jason M <46291780+JasonMorta@users.noreply.github.com>
2022-11-27 23:47:02 +0200

    updates

--- src/components/builds/expandingSections/CardsObj.jsx
+++ src/components/builds/expandingSections/CardsObj.jsx
@@ -8,17 +8,21 @@ export default [
     {
         id: 1,
         image: image1,
+        txt: "Morta Green"
     },   
     {
         id: 2,
         image: image2,
+        txt: "Green House"
     },   
     {
         id: 3,
         image: image3,
+        txt: "The Wild"
     },   
     {
         id: 4,
         image: image4,
+        txt: "Bushes"
     }
 ]
--- src/components/builds/expandingSections/Expand.jsx
+++ src/components/builds/expandingSections/Expand.jsx
@@ -9,6 +9,15 @@ export default function Expand() {
     const [active, setActive] = useState(false)
     let main = document.getElementsByClassName('container')
 
+
+    //timeout is needed, as nodeList 1st need to load in.
+    //preview one image on load
+    setTimeout(() => {
+      main[0].childNodes[1].classList.add('active')//image
+      main[0].childNodes[1].children[0].classList.add('activeText')
+
+    }, 200);
+
   return (
     <div className='container'>
         {images.map((ig, i)=>(
@@ -17,17 +26,27 @@ export default function Expand() {
                 style={{"backgroundImage": `url(${ig.image})`}}
                 key={ig.id}
                 onClick={function toggleClass(e){
-               
+               console.log(main);
+
+               //active text
+               main[0].childNodes.forEach(x =>
+                x.children[0].classList.remove('activeText')
+                )
+             
+
+               //on click remove the active className from every sibling div
                     main[0].childNodes.forEach(r => 
                         r.classList.remove('active'))
 
                     e.target.className === "content" ? e.target.className = "content active": 
                     e.target.className === "content active" ? e.target.className = "content" : console.log();
-             
-                   
+
+                    e.target.children[0].className === 'text' ? e.target.children[0].className = 'text activeText': 
+                    e.target.children[0].className === 'text activeText' ? e.target.children[0].className = 'text': console.log();
                 }
               }
                 >
+                  <h1 className='text'>{ig.txt}</h1>
             </div>
         ))}
     </div>
