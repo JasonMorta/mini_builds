5b2e9c2764b135044504cc2edab66c8efef61024
Jason M <46291780+JasonMorta@users.noreply.github.com>
2022-12-17 21:18:03 +0200

    updates

--- src/components/builds/IntersectionOP/IntersectionOP.jsx
+++ src/components/builds/IntersectionOP/IntersectionOP.jsx
@@ -47,12 +47,12 @@ export default function IntersectionOP() {
     
     <div ref={section4} className='contents section4'>
        {isVis4 ? <>
-            <h3 className='slide-in-left'>section4</h3>
+            <h3 className='slide-in-left '>section4</h3>
             <p className='slide-in-left'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
        </> :<></>}
     </div>
     <div ref={section5} className='contents section5'>
-        {isVis5 ? <h3 className='slide-in-elliptic-bottom-fwd'>END</h3> : <></>}
+        {isVis5 ? <h1 className='slide-in-elliptic-bottom-fwd '>END</h1> : <></>}
         
     </div>
     </section>
