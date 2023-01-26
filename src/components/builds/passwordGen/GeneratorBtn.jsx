5cdbf77c3dcdcf1264edfebc14e693e760573487
Jason M <46291780+JasonMorta@users.noreply.github.com>
2023-01-27 00:04:37 +0200

    updates

--- src/components/builds/passwordGen/GeneratorBtn.jsx
+++ src/components/builds/passwordGen/GeneratorBtn.jsx
@@ -9,34 +9,43 @@ export default function GeneratorBtn(props) {
 
     const value = useContext(StateContext)
     //destructure main state
-    let [options, setOptions] = value
+    const [options, setOptions] = value
+    let passWord = ""
 
 
 
     //Generate pass btn
 function makePass(){
+  console.log(passWord.length);
     // options.pass = "hello"
     // setPass(options.pass)
-    console.log(count);
-
-    for (let i = 0; i < count; i++) {
-        if (numbers) {
-            
-        }
-    }
-
-    function randomGen(lo, hi) {
-       return Math.floor((Math.random() * hi ) + lo)
-    }
-    
-  
-     
+    //console.log(count);
+    passWord = ""
+    for (let i = 0; i < count/2; i++) {
    
+
+        if (numbers) {
+          passWord += randomGen(0, 9)
+        }
+
+        if (lowerCase) {
+          passWord +=  String.fromCharCode(randomGen(97, 122))
+        }
+        if (count === passWord.length) {
+          break
+         }
+    }
+
     //props.data.pass
     //props.data.symbols
     //props.data.upperCase
+    console.log(passWord);
   }
 
+
+  function randomGen(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min)
+ }
 
   return (
     <div>
